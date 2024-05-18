919f7993d257411b6e080097e01093201f65bba3
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-05-18 18:15:59 +0530

    added themes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,7 +1,11 @@
-return{
-    { "EdenEast/nightfox.nvim"},
-    { "bluz71/vim-nightfly-colors", name = "nightfly"},
-    {"marko-cerovac/material.nvim"},
-    {"loctvl842/monokai-pro.nvim"},
-    {"Mofiqul/dracula.nvim"}
+return {
+  { "EdenEast/nightfox.nvim" },
+  { "bluz71/vim-nightfly-colors", name = "nightfly" },
+  { "marko-cerovac/material.nvim" },
+  { "loctvl842/monokai-pro.nvim" },
+  { "Mofiqul/dracula.nvim" },
+  { "projekt0n/github-nvim-theme" },
+  { "olimorris/onedarkpro.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "oxfist/night-owl.nvim" },
 }
