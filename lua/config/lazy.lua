96997e74942b1a0258d6a215f608e0f457078986
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-06-02 20:45:52 +0530

    added some github copilot code as comment

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,7 +24,6 @@ require("lazy").setup({
     -- Themes are coming from colorschemes.lua
     -- Below Is Code Runner Extension
     { "CRAG666/code_runner.nvim", config = true },
-    --{ "github/copilot.vim" },
     --Temporary fix for the breaking changes issue
     --Issue resolved ( just update your neovim to version 0.10 (stable))
     -- {
@@ -32,6 +31,112 @@ require("lazy").setup({
     --   event = "LazyFile",
     --   version = "=3.5.4",
     -- },
+
+    ----- TO USE COPILOT UNCOMMENT BELOW LINES -------
+    -- { "github/copilot.vim" },
+    -- Use <tab> for completion and snippets (supertab)
+    -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+    -- {
+    --   "L3MON4D3/LuaSnip",
+    --   keys = function()
+    --     return {}
+    --   end,
+    -- },
+    -- -- then: setup supertab in cmp
+    -- {
+    --   "hrsh7th/nvim-cmp",
+    --   dependencies = {
+    --     "hrsh7th/cmp-emoji",
+    --   },
+    --   ---@param opts cmp.ConfigSchema
+    --   opts = function(_, opts)
+    --     local has_words_before = function()
+    --       unpack = unpack or table.unpack
+    --       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    --       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    --     end
+    --
+    --     local luasnip = require("luasnip")
+    --     local cmp = require("cmp")
+    --
+    --     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+    --       ["<Tab>"] = cmp.mapping(function(fallback)
+    --         local copilot_keys = vim.fn["copilot#Accept"]()
+    --         if cmp.visible() then
+    --           cmp.select_next_item()
+    --         elseif luasnip.expand_or_jumpable() then
+    --           luasnip.expand_or_jump()
+    --         elseif copilot_keys ~= "" and type(copilot_keys) == "string" then
+    --           vim.api.nvim_feedkeys(copilot_keys, "i", true)
+    --         else
+    --           fallback()
+    --         end
+    --       end, { "i", "s" }),
+    --       ["<S-Tab>"] = cmp.mapping(function(fallback)
+    --         if cmp.visible() then
+    --           cmp.select_prev_item()
+    --         elseif luasnip.jumpable(-1) then
+    --           luasnip.jump(-1)
+    --         else
+    --           fallback()
+    --         end
+    --       end, { "i", "s" }),
+    --     })
+    --   end,
+    -- },
+
+    ----------UNCOMMENT ABOVE LINES TO USE COPILOT------------
+
+    {
+      "L3MON4D3/LuaSnip",
+      keys = function()
+        return {}
+      end,
+    },
+    -- then: setup supertab in cmp
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = {
+        "hrsh7th/cmp-emoji",
+      },
+      ---@param opts cmp.ConfigSchema
+      opts = function(_, opts)
+        local has_words_before = function()
+          unpack = unpack or table.unpack
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+
+        local luasnip = require("luasnip")
+        local cmp = require("cmp")
+
+        opts.mapping = vim.tbl_extend("force", opts.mapping, {
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- this way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        })
+      end,
+    },
+
     {
       "christoomey/vim-tmux-navigator",
       cmd = {
