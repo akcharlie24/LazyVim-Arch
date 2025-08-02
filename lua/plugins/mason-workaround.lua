0677ba938ee1272a06d882e344bfc1144a803c26
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2025-08-02 20:03:10 +0530

    fix: mason workaround

---
+++ lua/plugins/mason-workaround.lua
@@ -0,0 +1,5 @@
+-- This is a temporary fix fot the mason issue that is occuring, try and remove later on to see if fixed
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
